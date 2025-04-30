9dd679bcd3549257d977c025c7a714dc8dee1a3d
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-04-30 23:57:04 +0500

    Replace Hello World header with a root div in index.html and update script.js to use ReactDOM for rendering a new header component.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-import React from "react";
 
-console.log(React);
+import ReactDOM from 'react-dom/client';
+// console.log(React);
 
-if(module.hot){
-    module.hot.accept()
-}
+// if(module.hot){
+//     module.hot.accept()
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const h1 = <h1>Hello Parcel</h1>;
+console.log(h1)
